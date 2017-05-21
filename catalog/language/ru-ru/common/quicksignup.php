4d221749e194fe31d97fb378af77dafefb355bf7
jamal87 <jamal87@inbox.ru>
2017-05-21 20:17:03 +0300

    oc
    oc project

---
+++ catalog/language/ru-ru/common/quicksignup.php
@@ -0,0 +1,35 @@
+<?php
+// Text
+$_['signin_or_register'] = 'Вход | Регистрация';
+
+$_['text_signin_register'] = 'Вход | Регистрация';
+
+$_['text_new_customer'] = 'Новый клиент';
+$_['text_returning'] = 'Зарегистрированный клиент';
+$_['text_returning_customer'] = '';
+$_['text_details'] = '';
+$_['entry_type'] = 'Тип';
+$_['type_user'] = 'Частное лицо';
+$_['type_company'] = 'Компания';
+$_['entry_email'] = 'Email';
+$_['entry_name'] = 'Имя';
+$_['entry_password'] = 'Пароль';
+$_['entry_reply_password'] = 'Повторите пароль';
+$_['entry_telephone'] = 'Телефон';
+$_['text_forgotten'] = 'Забыли пароль?';
+$_['text_agree'] = 'Я прочел и согласен с <a href="%s" class="agree"><b>%s</b></a>';
+
+//Button
+$_['button_login'] = 'Войти';
+
+//Error
+$_['error_name'] = 'Имя должно иметь от 1 до 32 символов!';
+$_['error_email'] = 'E-Mail некорректный!';
+$_['error_telephone'] = 'Телефон должен иметь от 3 до 32 символов!';
+$_['error_password'] = 'Пароль должен иметь от 4 до 20 символов!';
+$_['error_reply_password'] = 'Пароль не совпадает с введенным ранее!';
+$_['error_exists'] = 'Внимание: этот e-mail уже используется!';
+$_['error_agree'] = 'Внимание: Вы должны согласиться с %s!';
+$_['error_warning'] = 'Внимание: проверьте форму на ошибки!';
+$_['error_approved'] = 'Внимание: Ваш аккаунт должен быть одобрен прежде, чем Вы сможете войти в кабинет.';
+$_['error_login'] = 'Внимание: не подходит e-mail и/или пароль.';
